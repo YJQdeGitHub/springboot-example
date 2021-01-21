6c196364c54c9f55562836870b1670611f74f8ee
YJQdeGitHub <y15011404642@163.com>
2021-01-21 17:46:26 +0800

    01.21

---
+++ springboot-web/src/main/java/com/example/web/util/NeoProperties.java
@@ -0,0 +1,30 @@
+package com.example.web.util;
+
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * @ClassName NeoProperties
+ * @Description TODO
+ * @Author yangjunqi
+ * @Date 2021/1/21 17:37
+ * @Version 1.0
+ **/
+public class NeoProperties {
+
+    @Value("${com.neo.title}")
+    private String title;
+    @Value("${com.neo.description}")
+    private String description;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
